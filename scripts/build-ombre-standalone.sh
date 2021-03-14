95912cd53a19eaf1e4c6612ef15351ffac13ae24
jinfeixibi990910 <phk990910@gmail.com>
2021-03-14 12:38:05 +0800

    delete

--- scripts/build-ombre-standalone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Loading binary/specs variables
-
-docker build -f docker/ombre-standalone.Dockerfile \
-  -t shadowsnetwork/ombre-standalone:lasest .
--- tests/tests/util/index.ts
+++
@@ -1,9 +0,0 @@
-export { startShadowsNode, describeWithShadows } from "./testWithShadows";
-export { fillBlockWithTx } from "./fillBlockWithTx";
-export {
-  customRequest,
-  wrappedCustomRequest,
-  deployContractManualSeal,
-  callContractFunctionMS,
-} from "./web3Requests";
-export { createAndFinalizeBlock } from "./polkadotApiRequests";
